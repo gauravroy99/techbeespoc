f8922786528663c14c010f9e901a45c39d9f0452
gauravroy99 <74399191+gauravroy99@users.noreply.github.com>
2020-11-15 07:27:06 +0530

    Update UIConstants.java
    Now code is following the naming convention for constants.

--- src/com/constants/UIConstants.java
+++ src/com/constants/UIConstants.java
@@ -2,25 +2,25 @@ package com.constants;
 
 public class UIConstants {
 	
-	public static final String loginEmail = "Email";
-	public static final String loginPassword = "Password";
-	public static final String loginSuccess = "success";
-	public static final String loginUsernameIncorrect = "username_incorrect";
+	public static final String LOGIN_EMAIL = "Email";
+	public static final String LOGIN_PASSWORD = "Password";
+	public static final String LOGIN_SUCCESS = "success";
+	public static final String LOGIN_USERNAME_INCORRECT = "username_incorrect";
 	
 	//forget page UI Fields
-	public static final String forgetNumber = "mobile_number";
+	public static final String FORGET_NUMBER = "mobile_number";
 	
 	//session Attributes
-	public static final String sessionName  = "name";
-	public static final String sessionIsAdmin = "isAdmin";
-	public static final String sessionLoggedIn  = "LoggedIn";
-	public static final String sessionUser_id="ID";
+	public static final String SESSION_NAME  = "name";
+	public static final String SESSION_ISADMIN = "isAdmin";
+	public static final String SESSION_LOGGEDIN  = "LoggedIn";
+	public static final String SESSION_USER_ID="ID";
 	
-	public static final String FIRSTNAME  = "First_Name";
-	public static final String LASTNAME  = "Last_Name";
-	public static final String EMAILID  = "Email_Id";
-	public static final String MOBILENUMBER  = "Mobile_Number";
-	public static final String DATEOFBIRTH  = "DOB";
+	public static final String FIRST_NAME  = "First_Name";
+	public static final String LAST_NAME  = "Last_Name";
+	public static final String EMAIL_ID  = "Email_Id";
+	public static final String MOBILE_NUMBER  = "Mobile_Number";
+	public static final String DATE_OF_BIRTH  = "DOB";
 	public static final String GENDER  = "gender";
 	public static final String ADDRESS  = "Address";
 	public static final String CITY  = "City";
@@ -30,6 +30,6 @@ public class UIConstants {
 	
 	
 	
-	public static final String DELETEUSINGSEARCH  = "DeleteUsingSearch.jsp";
+	public static final String DELETE_USING_SEARCH  = "DeleteUsingSearch.jsp";
 	
 }
